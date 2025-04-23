d9d919a378b8ff74a9badc5aa490c6814d0459cf
gunnar larsen <gunnarrl@hawaii.edu>
2025-04-23 08:41:16 -1000

    undoing previous change

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
+import authOptions from '@/lib/authOptions';
 
 const handler = NextAuth(authOptions);
-export default { GET: handler, POST: handler };
+export { handler as GET, handler as POST };
